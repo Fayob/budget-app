c8c170c55e90804c515aefd7553bb51e08fdfddc
fayob <adedejifavour6@gmail.com>
2023-02-13 14:26:05 +0100

    Add association to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :categories, dependent: :delete_all
+  has_many :transacts, dependent: :delete_all
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 70 }
 end
