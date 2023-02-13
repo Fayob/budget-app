562ba178cae41ef0571445696ca3ffa32645e5ac
fayob <adedejifavour6@gmail.com>
2023-02-13 20:08:17 +0100

    Fix rubocop error

--- app/models/transact.rb
+++ app/models/transact.rb
@@ -3,5 +3,5 @@ class Transact < ApplicationRecord
   has_many :category_transacts
 
   validates :name, presence: true
-  validates :amount, numericality: {greater_than_or_equal_to: 0}
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
