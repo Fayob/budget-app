a9e5080da400d796eb02b346d60cf56076e18a89
fayob <adedejifavour6@gmail.com>
2023-02-13 20:06:26 +0100

    Add devise and cancancan gem to app

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,10 +1,6 @@
-class User < ApplicationRecord
+class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :categories, dependent: :delete_all
-  has_many :transacts, dependent: :delete_all
-
-  validates :name, presence: true, length: { minimum: 2, maximum: 70 }
 end
