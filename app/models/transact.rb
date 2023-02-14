2eaf2540083707c55ffd188a2c6e9924bfe32500
fayob <adedejifavour6@gmail.com>
2023-02-14 21:12:29 +0100

    Add more validation to transact model

--- app/models/transact.rb
+++ app/models/transact.rb
@@ -1,7 +1,8 @@
 class Transact < ApplicationRecord
   belongs_to :user
   has_many :category_transacts
+  has_many :categories, through: :category_transacts
 
   validates :name, presence: true
-  validates :amount, numericality: { greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: { only_numeric: true, greater_than_or_equal_to: 0 }
 end
