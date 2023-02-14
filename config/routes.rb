234d6603b50096d06864fc3b0da5a48bcec96f95
fayob <adedejifavour6@gmail.com>
2023-02-14 21:11:04 +0100

    Add categories and transacts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#home'
+  root 'categories#index'
+  resources :categories do
+    resources :transacts
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
