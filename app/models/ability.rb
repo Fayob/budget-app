585cb45f5b8f21b7f2d575c6aeb8fddccf24ec60
fayob <adedejifavour6@gmail.com>
2023-02-16 17:37:49 +0100

    Set authorization manager

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,12 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+
+    return unless user.present?
+
+    can :manage, Category, user_id: user.id
+    can :manage, Transact, user_id: user.id
+    
+  end
 end
