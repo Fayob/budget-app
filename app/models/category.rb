85b3c384448e4fadcde2ce2aec3e60a5dca14fc3
fayob <adedejifavour6@gmail.com>
2023-02-18 23:25:38 +0100

    Add dependent to many relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :category_transacts
-  has_many :transacts, through: :category_transacts
+  has_many :category_transacts, dependent: :delete_all
+  has_many :transacts, through: :category_transacts, dependent: :delete_all
 
   validates :name, presence: true
   validates :icon, presence: true
--- app/models/transact.rb
+++ app/models/transact.rb
@@ -1,7 +1,7 @@
 class Transact < ApplicationRecord
   belongs_to :user
-  has_many :category_transacts
-  has_many :categories, through: :category_transacts
+  has_many :category_transacts, dependent: :delete_all
+  has_many :categories, through: :category_transacts, dependent: :delete_all
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { only_numeric: true, greater_than_or_equal_to: 0 }
